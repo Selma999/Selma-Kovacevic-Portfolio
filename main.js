992967c2a2608406f3c49ff89cfb85244ce89758
enver1473 <enverkovacevic@hotmail.com>
2021-04-21 11:26:56 +0200

    Animation updates

--- main.js
+++ main.js
@@ -1,29 +1,44 @@
-$(document).ready(function() {
-    $('.menu-toggler').on('click', function() {
-        $(this).toggleClass('change');
-        $('.top-nav').toggleClass('change');
-    });
+$(document).ready(function () {
+  $(".menu-toggler").on("click", function () {
+    $(this).toggleClass("change");
+    $(".top-nav").toggleClass("change");
+    $(".nav-overlay").toggleClass("active");
 
-    $('.top-nav .nav-link').on('click', function() {
-        $('.menu-toggler').removeClass('change');
-        $('.top-nav').removeClass('change');
-    });
+    if ($(".top-nav").hasClass("change")) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  });
 
-    $('nav a[href*="#"').on('click', function() {
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top
-        }, 1000);
-    });
+  $(".top-nav .nav-link").on("click", function () {
+    $(".menu-toggler").removeClass("change");
+    $(".top-nav").removeClass("change");
+    $(".nav-overlay").removeClass("active");
+    document.body.style.overflow = "auto";
+  });
 
-    $('#up').on('click', function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 800);
-    });
+  $('nav a[href*="#"').on("click", function () {
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top,
+      },
+      1000
+    );
+  });
 
-    AOS.init({
-        easing: 'ease',
-        duration: 1800,
-        once: true
-    })
+  $("#up").on("click", function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      800
+    );
+  });
+
+  AOS.init({
+    easing: "ease",
+    duration: 1800,
+    once: true,
+  });
 });
